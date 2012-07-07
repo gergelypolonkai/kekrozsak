837d93b14c6ebac7f605109f61c8202897cd2be1
Polonkai Gergely <polesz@w00d5t0ck.info>
2012-07-07 20:56:04 +0200

    Now redirecting logged in users to main page from registration form

--- src/KekRozsak/SecurityBundle/Controller/DefaultController.php
+++ src/KekRozsak/SecurityBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace KekRozsak\SecurityBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 use KekRozsak\FrontBundle\Entity\User;
 use KekRozsak\FrontBundle\Form\Type\UserType;
@@ -34,6 +35,11 @@ class DefaultController extends Controller
 
 	public function registrationAction(Request $request)
 	{
+		$user = $this->get('security.context')->getToken()->getUser();
+		if ($user instanceof UserInterface)
+		{
+			return $this->redirect($this->generateUrl('KekRozsakFrontBundle_homepage'));
+		}
 		$user = new User();
 		$form = $this->createForm(new UserType(true), $user);
 
