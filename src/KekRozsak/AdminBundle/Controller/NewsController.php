91e5d2336c25c35bfb5394ce0734f3a4be4f5b84
Gergely POLONKAI (W00d5t0ck) <polesz@w00d5t0ck.info>
2012-09-01 23:54:47 +0200

    Added skeleton for news editor
    Signed-off-by: Gergely POLONKAI (W00d5t0ck) <polesz@w00d5t0ck.info>

--- src/KekRozsak/AdminBundle/Controller/NewsController.php
+++ src/KekRozsak/AdminBundle/Controller/NewsController.php
@@ -4,9 +4,12 @@ namespace KekRozsak\AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
 use JMS\DiExtraBundle\Annotation as DI;
+
+use KekRozsak\FrontBundle\Entity\News;
 
 /**
  * @Route("/admin")
@@ -35,17 +38,37 @@ class NewsController extends Controller
     private $securityContext;
 
     /**
+     * @var Symfony\Component\Security\Acl\Domain\ObjectIdentity $objectIdentity
+     */
+
+    public function __construct()
+    {
+        $this->objectIdentity = new ObjectIdentity(self::OBJECT_ID, self::OBJECT_FQCN);
+    }
+
+    /**
      * @Route("/hirek/", name="KekRozsakAdminBundle_newsList")
      * @Template
      */
     public function listAction()
     {
-        $objectIdentity = new ObjectIdentity(self::OBJECT_ID, self::OBJECT_FQCN);
-        if ($this->securityContext->isGranted('OWNER', $objectIdentity) === false) {
+        if ($this->securityContext->isGranted('OWNER', $this->objectIdentity) === false) {
             throw new AccessDeniedException('Nincs jogosultságod a hírszerkesztéshez!');
         }
         $news = $this->getDoctrine()->getRepository('KekRozsakFrontBundle:News')->findAll();
 
+        return array(
+            'news' => $news,
+        );
+    }
+
+    /**
+     * @Route("/hirek/{slug}/szerkesztes.html", name="KekRozsakAdminBundle_newsEdit")
+     * @Template
+     * @ParamConverter("news")
+     */
+    public function editAction(News $news)
+    {
         return array(
             'news' => $news,
         );
